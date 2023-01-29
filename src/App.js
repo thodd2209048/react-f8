12418b153ae5b80b96c4aee42e8da404ca46f6d7
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2023-01-29 19:47:22 +0700

    useReducer, up/down a number

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import { useState } from "react";
+import { useCallback, useMemo, useReducer, useRef, useState } from "react";
 import Content from "./Content";
-
+const initState = 0;
+const UP_ACTION = "up";
+const DOWN_ACTION = "down";
+const reducer = (state, action) => {
+  switch (action) {
+    case UP_ACTION:
+      return state + 1;
+    case DOWN_ACTION:
+      return state - 1;
+    default:
+      throw new Error("invalid action");
+  }
+};
 function App() {
-  const [show, setShow] = useState(false);
+  const [count, dispatch] = useReducer(reducer, initState);
   return (
-    <div>
-      <button onClick={() => setShow(!show)}>Toggle</button>
-      {show && <Content />}
+    <div style={{ padding: "10px 32px" }}>
+      <h1>{count}</h1>
+      <button onClick={() => dispatch(UP_ACTION)}>Up</button>
+      <button onClick={() => dispatch(DOWN_ACTION)}>Down</button>
     </div>
   );
 }
--- src/Content.js
+++ src/Content.js
@@ -1,36 +1,13 @@
-import { useEffect, useState } from "react";
-const lessons = [
-  { id: 1, name: "ReactJs" },
-  { id: 2, name: "SPA" },
-  { id: 3, name: "Arrow function" },
-];
-function Content() {
-  const [lessonId, setLessonId] = useState(1);
-  useEffect(() => {
-    const handleComment = (e) => {
-      console.log(e);
-    };
-    window.addEventListener(`lesson-${lessonId}`, handleComment);
+import { memo, useEffect, useRef, useState } from "react";
 
-    return () => {
-      window.removeEventListener(`lesson-${lessonId}`, handleComment);
-    };
-  }, [lessonId]);
+function Content({ onIncrease }) {
+  console.log("re-render");
   return (
-    <div>
-      <ul>
-        {lessons.map((lesson) => (
-          <li
-            key={lesson.id}
-            style={{ color: lessonId === lesson.id ? "red" : "#ccc" }}
-            onClick={() => setLessonId(lesson.id)}
-          >
-            {lesson.name}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <h2>HELLO ANH EM</h2>
+      <button onClick={onIncrease}>Click me</button>
+    </>
   );
 }
 
-export default Content;
+export default memo(Content);
